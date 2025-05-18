d8036dcae03ab4a0c26f5889ef00fd383c08df9e
uLinck <linck.privado@gmail.com>
2025-05-18 03:21:52 -0300

    feat: implementing entities and a migrations tutorial txt

---
+++ BackEnd/src/FlavorTalk.Domain/Entities/Merchant.cs
@@ -0,0 +1,17 @@
+﻿namespace FlavorTalk.Domain.Entities;
+public class Merchant : UserBaseEntity
+{
+    public required string Name { get; set; }
+
+    // TO-DO Catalog
+
+    // TO-DO Review
+
+    public decimal? RatingAverage { get; set; }
+
+    public long Likes { get; set; } = 0;
+
+    public required string ContactNumber { get; set; }
+
+    public required Address Address { get; set; }
+}
--- BackEnd/src/FlavorTalk.Infrastructure/Data/FlavorTalkContext.cs
+++ BackEnd/src/FlavorTalk.Infrastructure/Data/FlavorTalkContext.cs
@@ -24,6 +24,8 @@ public class FlavorTalkContext : IdentityDbContext<User, Role, Guid>
         // Override default AspNet Identity table names
         modelBuilder.Entity<User>(entity => entity.ToTable(name: "Users"));
         modelBuilder.Entity<Role>(entity => entity.ToTable(name: "Roles"));
+        modelBuilder.Entity<Merchant>(merchant => merchant.ToTable(name: "Merchant"));
+
         modelBuilder.Entity<IdentityUserRole<Guid>>(entity => entity.ToTable("UserRoles"));
         modelBuilder.Entity<IdentityUserClaim<Guid>>(entity => entity.ToTable("UserClaims"));
         modelBuilder.Entity<IdentityUserLogin<Guid>>(entity => entity.ToTable("UserLogins"));
